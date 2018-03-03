d0f4b43ba336ee559e9bede841e0bcd56e07bdf4
cppchriscpp <admin@cpprograms.net>
2018-03-03 15:37:56 -0500

    Fix hud color bug

--- source/map/map.c
+++ source/map/map.c
@@ -40,10 +40,16 @@ void init_map() {
     xScrollPosition = -1;
 }
 
-void clear_asset_table() {
+// Clears the asset table. Set containsHud to 1 to set the HUD bytes to use palette 4 (will break the coloring logic if you use the
+// last few rows for the map.)
+void clear_asset_table(containsHud) {
     // Loop over assetTable to clear it out. 
-    for (i = 0; i != sizeof(assetTable); ++i) {
+    for (i = 0; i != sizeof(assetTable) - 8; ++i) {
         assetTable[i] = 0x00;
+    }
+    // The last row of the asset table uses the 4th palette to show the HUD correctly.
+    for (; i != sizeof(assetTable); ++i) {
+        assetTable[i] = containsHud == 0 ? 0x00 : 0xff;
     }
 }
 
@@ -57,7 +63,6 @@ void draw_current_map_to_nametable(int nametableAdr, int attributeTableAdr, unsi
     vram_inc(0);
     set_vram_update(NULL);
 
-    clear_asset_table();
     if (!reverseAttributes) {
         j = -1;
     } else {
@@ -261,18 +266,22 @@ void draw_individual_row(int nametableAdr, int attributeTableAdr, char oliChange
 }
 
 void draw_current_map_to_a() {
+    clear_asset_table(1);
     draw_current_map_to_nametable(NAMETABLE_A, NAMETABLE_A + 0x3c0, 0);
 }
 
 void draw_current_map_to_b() {
+    clear_asset_table(0);
     draw_current_map_to_nametable(NAMETABLE_B, NAMETABLE_B + 0x3c0, 0);
 }
 
 void draw_current_map_to_c() {
+    clear_asset_table(0);
     draw_current_map_to_nametable(NAMETABLE_C, NAMETABLE_C + 0x3c0, 0);
 }
 
 void draw_current_map_to_d() {
+    clear_asset_table(0);
     draw_current_map_to_nametable(NAMETABLE_D, NAMETABLE_D + 0x3c0, 0);
 }
 
@@ -288,6 +297,7 @@ void do_screen_scroll() {
     if (playerDirection == SPRITE_DIRECTION_RIGHT) {
         load_map();
 
+        clear_asset_table(1);
         draw_current_map_to_nametable(NAMETABLE_B, NAMETABLE_B + 0x3c0, 0);
         // TODO: Move and animate player sprite somehow
         for (i = 0; i != 254; i+=2) {
@@ -299,6 +309,7 @@ void do_screen_scroll() {
     } else if (playerDirection == SPRITE_DIRECTION_LEFT) {
         load_map();
 
+        clear_asset_table(1);
         draw_current_map_to_nametable(NAMETABLE_B, NAMETABLE_B + 0x3c0, 0);
         // TODO: Move and animate player sprite somehow
         for (i = 0; i != 254; i+=2) { // we depend on i being an 8 bit integer here (values from 0-255), so 0 rolls over to 254.
@@ -309,18 +320,18 @@ void do_screen_scroll() {
         xScrollPosition = 256;
     } else if (playerDirection == SPRITE_DIRECTION_DOWN) {
         // First draw original map to the other nametable
+        clear_asset_table(0);
         draw_current_map_to_nametable(NAMETABLE_B + (SCREEN_WIDTH_TILES*6), NAMETABLE_B + 0x3c0 + 8, 1);
         
         load_map();
         // Loop over the screen, drawing the map in the space taken up by the hud every time we go 32 lines (2 tiles)
         // NOTE: We use both i and j in the loop inside one of the functions we're calling, so we needed another variable.
-        clear_asset_table();
+        clear_asset_table(0);
         i = 0; 
         j = -1;
         xScrollPosition = 256;
         for (otherLoopIndex = 0; otherLoopIndex < 243 - HUD_PIXEL_HEIGHT; otherLoopIndex+=2) {
             if (otherLoopIndex % 32 == 0 && otherLoopIndex < 224) {
-                bufferIndex = 0; // TODO: Needed?
                 draw_individual_row(NAMETABLE_B, NAMETABLE_B + 0x3c0, 2);
             } else {
                 ppu_wait_nmi();
@@ -332,11 +343,12 @@ void do_screen_scroll() {
         xScrollPosition = 256;
     } else if (playerDirection == SPRITE_DIRECTION_UP) {
         // First draw original map to the other nametable
+        clear_asset_table(0);
         draw_current_map_to_nametable(NAMETABLE_B + (SCREEN_WIDTH_TILES*6), NAMETABLE_B + 0x3c0 + 8, 1);
         load_map();
         // Loop over the screen, drawing the map in the space taken up by the hud every time we go 32 lines (2 tiles)
         // NOTE: We use both i and j in the loop inside one of the functions we're calling, so we needed another variable.
-        clear_asset_table();
+        clear_asset_table(0);
         i = 0; 
         j = -1;
         xScrollPosition = 256;
@@ -344,7 +356,6 @@ void do_screen_scroll() {
         // Since we're using an unsigned char, 0-1 = 255, so as soon as we get below zero the loop terminates.
         for (otherLoopIndex = 242 - HUD_PIXEL_HEIGHT; otherLoopIndex < 242; otherLoopIndex-=2) {
             if (otherLoopIndex % 32 == 0 && otherLoopIndex != 0) {
-                bufferIndex = 0; // TODO: Needed?
                 // TODO: Need to figure out how to make this work in reverse order. (Mess with i and j, I assume)
                 draw_individual_row(NAMETABLE_B, NAMETABLE_B + 0x3c0, -2);
             } else {
@@ -362,6 +373,7 @@ void do_screen_scroll() {
     playerXPosition = playerYPosition = 2500;
 
     // Now, draw back to our original nametable...
+    clear_asset_table(1);
     draw_current_map_to_nametable(NAMETABLE_A, NAMETABLE_A + 0x3c0, 0);
 
     // and bump the player back to the first screen now that we're done.
