749bfc1f7fc8159c2aeb80028ac7052595ca4c76
cppchriscpp <admin@cpprograms.net>
2018-04-27 21:58:04 +0100

    So close...

--- .circleci/set_up_tools.sh
+++ .circleci/set_up_tools.sh
@@ -2,7 +2,7 @@
 # Grab it from the interbuttz, then extract it and build it using gcc 
 if [ ! -f tools/cc65/bin/cc65 ]; then mkdir -p tools/cc65-temp; fi
 if [ ! -f tools/cc65/bin/cc65 ]; then cd tools/cc65-temp/ && wget ftp://ftp.musoftware.de/pub/uz/cc65/cc65-sources-2.13.3.tar.bz2 && tar xvjf cc65-sources-2.13.3.tar.bz2 && cd ../..; fi
-if [ ! -f tools/cc65/bin/cc65 ]; then cd tools/cc65-temp/cc65-2.13.3 && make --file=make/gcc.mak && cd ../..; fi
+if [ ! -f tools/cc65/bin/cc65 ]; then cd tools/cc65-temp/cc65-2.13.3 && make --file=make/gcc.mak && cd ../../..; fi
 if [ ! -f tools/cc65/bin/cc65 ]; then mv tools/cc65-temp/cc65-2.13.3/* tools/cc65; fi
 # Create a bin dir, then copy all the files to it.
 mkdir -p tools/cc65/bin
