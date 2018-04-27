28d9eb87eeb9eede11af3d0dbbe81eb69747791e
cppchriscpp <admin@cpprograms.net>
2018-04-27 21:56:03 +0100

    Tweak script a little from old circleci weirdness

--- .circleci/set_up_tools.sh
+++ .circleci/set_up_tools.sh
@@ -1,9 +1,9 @@
 # Install cc65 if it's not already in the cache.
 # Grab it from the interbuttz, then extract it and build it using gcc 
 if [ ! -f tools/cc65/bin/cc65 ]; then mkdir -p tools/cc65-temp; fi
-if [ ! -f tools/cc65/bin/cc65 ]; then cd tools/cc65-temp/ && wget ftp://ftp.musoftware.de/pub/uz/cc65/cc65-sources-2.13.3.tar.bz2 && tar xvjf cc65-sources-2.13.3.tar.bz2; fi
-if [ ! -f tools/cc65/bin/cc65 ]; then cd tools/cc65-temp/cc65-2.13.3 && make --file=make/gcc.mak; fi
-if [ ! -f tools/cc65/bin/cc65 ]; then  mv tools/cc65-temp/cc65-2.13.3/* tools/cc65; fi
+if [ ! -f tools/cc65/bin/cc65 ]; then cd tools/cc65-temp/ && wget ftp://ftp.musoftware.de/pub/uz/cc65/cc65-sources-2.13.3.tar.bz2 && tar xvjf cc65-sources-2.13.3.tar.bz2 && cd ../..; fi
+if [ ! -f tools/cc65/bin/cc65 ]; then cd tools/cc65-temp/cc65-2.13.3 && make --file=make/gcc.mak && cd ../..; fi
+if [ ! -f tools/cc65/bin/cc65 ]; then mv tools/cc65-temp/cc65-2.13.3/* tools/cc65; fi
 # Create a bin dir, then copy all the files to it.
 mkdir -p tools/cc65/bin
 find tools/cc65 -type f -executable -exec cp {} tools/cc65/bin/ \;
