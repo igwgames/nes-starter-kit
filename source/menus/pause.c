e954bde2a82bce60c587d4a300fe13117fbaf8a4
cppchriscpp <admin@cpprograms.net>
2018-07-19 20:39:55 -0400

    Add pause changes

--- source/menus/pause.c
+++ source/menus/pause.c
@@ -9,6 +9,7 @@
 
 CODE_BANK(PRG_BANK_PAUSE_MENU);
 
+#define currentMenuItem tempChar1
 
 void draw_pause_screen(void) {
     ppu_off();
@@ -22,9 +23,11 @@ void draw_pause_screen(void) {
     set_chr_bank_1(CHR_BANK_MENU);
 
     // Just write "- Paused -" on the screen... there's plenty of nicer things you could do if you wanna spend time!
-    put_str(NTADR_A(11, 13), "- Paused -");
+    put_str(NTADR_A(11, 6), "- Paused -");
 
-
+    put_str(NTADR_A(10, 18), "Continue");
+    put_str(NTADR_A(10, 20), "Trigger Game Over");
+    put_str(NTADR_A(10, 22), "Exit Game");
 
     // We purposely leave sprites off, so they do not clutter the view. 
     // This means all menu drawing must be done with background tiles - if you want to use sprites (eg for a menu item),
@@ -33,6 +36,70 @@ void draw_pause_screen(void) {
 }
 
 void handle_pause_input(void) {
-    banked_call(PRG_BANK_MENU_INPUT_HELPERS, wait_for_start);
+    currentMenuItem = 0;
+
+    // Fill screenBuffer with the information required to update the menu every tile - we'll then edit this
+    // array in-line each frame.
+    i = 0;
+    screenBuffer[i++] = MSB(NTADR_A(8, 18)) | NT_UPD_VERT;
+    screenBuffer[i++] = LSB(NTADR_A(8, 18));
+    screenBuffer[i++] = 6;
+    screenBuffer[i++] = MENU_ARROW_TILE;
+    for (; i != 9; ++i) {
+        screenBuffer[i] = MENU_BLANK_TILE;
+    }
+    screenBuffer[i] = NT_UPD_EOF;
+
+    set_vram_update(screenBuffer);
+
+    while (1) {
+        lastControllerState = controllerState;
+        controllerState = pad_poll(0);
+
+        // If Start is pressed now, and was not pressed before...
+        if (controllerState & PAD_START && !(lastControllerState & PAD_START)) {
+            break;
+        }
+
+        if (controllerState & PAD_UP && !(lastControllerState & PAD_UP)) {
+            if (currentMenuItem != 0) {
+                --currentMenuItem;
+            }
+        }
+
+        if (controllerState & PAD_DOWN && !(lastControllerState & PAD_DOWN)) {
+            if (currentMenuItem != 2) {
+                ++currentMenuItem;
+            }
+        }
+
+        if (controllerState & PAD_A && !(lastControllerState & PAD_A)) {
+            if (currentMenuItem == 0) { // Resume game
+                break;
+            } else if (currentMenuItem == 1) { // Game over!
+
+                gameState = GAME_STATE_GAME_OVER;
+
+                // We have to skip the rest of the function so we don't reset gameState. 
+                // Do the vram update so we don't forget.
+                set_vram_update(NULL);
+                return;
+            } else if (currentMenuItem == 2) { // Reset game
+                
+                // This function resets the entire game; use it with extreme caution.
+                // No code past this function call will run.
+                reset();
+            }
+        }
+
+        screenBuffer[3] = (currentMenuItem == 0 ? MENU_ARROW_TILE : MENU_BLANK_TILE);
+        screenBuffer[5] = (currentMenuItem == 1 ? MENU_ARROW_TILE : MENU_BLANK_TILE);
+        screenBuffer[7] = (currentMenuItem == 2 ? MENU_ARROW_TILE : MENU_BLANK_TILE);
+
+        ppu_wait_nmi();
+    }
+
+    // This is from our existing code, not `wait_for_start()` - don't forget it!
     gameState = GAME_STATE_RUNNING;
+    set_vram_update(NULL);
 }
