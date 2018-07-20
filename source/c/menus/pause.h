050fc878de32323d399d0d5d41054613ada7927b
cppchriscpp <admin@cpprograms.net>
2018-07-19 20:39:55 -0400

    Add pause changes

--- source/c/menus/pause.h
+++ source/c/menus/pause.h
@@ -1,6 +1,10 @@
 // Draws and handles a very simple pause screen.
 #define PRG_BANK_PAUSE_MENU 2
 
+// The tile to draw for the selected menu item.
+#define MENU_ARROW_TILE 0xe2
+// The tile to show for empty space (ascii tiles are 32 bytes off, so we have to remove that here)
+#define MENU_BLANK_TILE ' ' - 0x20
 
 void draw_pause_screen(void);
 
