3790619cfbe53dfbf18c8d4aed07920fb90357b1
cppchriscpp <admin@cpprograms.net>
2020-07-28 06:07:33 +0100

    Switch player movement to be guided on an 8px grid

--- source/sprites/player.h
+++ source/sprites/player.h
@@ -7,6 +7,15 @@
 // The id of the first sprite in the 4 sprites comprising the player. (out of 64 total sprites)
 #define PLAYER_SPRITE_INDEX 0x10
 
+// This is simple movement style that leaves everything up to the player.
+#define MOVEMENT_STYLE_LOOSE 0
+// This is a movement style that guides the player onto an 8px "grid", to make it harder to get
+// stuck on edges. 
+#define MOVEMENT_STYLE_GRID 1
+
+// This define changes how the player moves to be one of the two movement styles above.
+#define PLAYER_MOVEMENT_STYLE MOVEMENT_STYLE_GRID
+
 // This is the width and height of player as used in collisions. This is shifted by 4 to allow for sub-pixel
 // sizes. (And smoother acceleration/deceleration.) A 16px wide sprite is 256 units here.
 #define PLAYER_WIDTH_EXTENDED 205
@@ -18,11 +27,19 @@
 // by to get to the real location. (Think about this as dividing by 2^(this number))
 #define PLAYER_POSITION_SHIFT 4
 
+// This is used to help nudge the player when their velocity is 0 in a dimension.
+#define PLAYER_POSITION_TILE_MASK 0x0070
+// This is used to figure out where the middle of the grid is. It's half of the max value of the mask above.
+#define PLAYER_POSITION_TILE_MASK_MIDDLE 0x0040
+// On the Y axis, due to some oddities in the engine, we have to nudge things over by 1 pixel
+#define PLAYER_POSITION_TILE_MASK_EXTRA_NUDGE 0x10
+
 // Handles player movement - we start slow and accelerate/decelerate up to a max speed. As a player,
 // you probably won't notice this outright, but it feels a lot more natural like this.
 // NOTE: The max velocity needs to be evenly divided by the acceleration. (Eg 20/4 = 5)
 #define PLAYER_MAX_VELOCITY 20 // Max speed of the player; we won't let you go past this.
 #define PLAYER_VELOCITY_ACCEL 4 // How quickly do we get up to max velocity?
+#define PLAYER_VELOCITY_NUDGE 8 // How quickly do we "nudge" the player onto the grid? THIS MUST BE A MULTIPLE OF THE ABOVE NUMBER
 
 // If the player is damaged, how long do we lock their controls such that they bounce backwards?
 #define PLAYER_DAMAGE_CONTROL_LOCK_TIME 20
